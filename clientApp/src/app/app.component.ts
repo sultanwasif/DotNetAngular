e5fb4506c9183b27e00ffffaadba3621817175e4
sultanwasif <sultanwasif@gmail.com>
2022-12-11 14:28:42 -0500

    Angular Changes

---
+++ clientApp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  title = 'clientApp';
+  users : any;
+  
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:7115/api/users").subscribe({
+      next : response => this.users = response,
+      error : error => console.error(error),
+      complete : () => console.log("Completed")      
+    });
+  }
+}
